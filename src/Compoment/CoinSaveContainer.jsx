dee60c6e8568638600e7c1dffc51d3e0f2246dc1
Kaan <kaansahin3800@gmail.com>
2022-09-24 22:18:03 +0300

    commit

--- src/Compoment/CoinSaveContainer.jsx
+++ src/Compoment/CoinSaveContainer.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react'
+import { MainContext } from '../CreateContext'
 import './CoinSaveContainer.css'
-
+import { FaWindowClose } from 'react-icons/fa'
 const CoinSaveContainer = () => {
-
+const {active, setActive} = useContext(MainContext)
   return (
     <div className={`CoinSaveContainerKapsam ${active === 'CoinSaveContainerActive' ? 'active' : ''} `} >
           <div className='CoinSaveContainer'> 
-              
+            <div className='close-icon-container'>
+                <FaWindowClose className='icon' />
+            </div>
+               <h1>
+               GEÇİÇİ SÜRE BAKIMA ALINMIŞTIR...
+               </h1>
           </div>
     </div>
   )
